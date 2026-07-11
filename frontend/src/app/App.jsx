6c4dcbaa4596f8449a43145cfe5eeee17c993e0f
donald <warnerroby6@gmail.com>
2026-07-11 19:58:03 +0545

    created socket connection at the frontend side as well

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,8 +1,32 @@
 import "./App.css";
-
 import { Editor } from "@monaco-editor/react";
+import { MonacoBinding } from "y-monaco";
+import { useRef, useMemo } from "react";
+import * as Y from "yjs";
+import { SocketIOProvider } from "y-socket.io";
 
 function App() {
+  const editorRef = useRef(null);
+
+  const ydoc = useMemo(() => new Y.Doc(), []);
+  const ytext = useMemo(() => ydoc.getText("monaco"), [ydoc]);
+
+  const handleMount = (editor) => {
+    editorRef.current = editor;
+    const provider = new SocketIOProvider(
+      "http://localhost:3000",
+      "monaco-room",
+      ydoc,
+      { autoConnect: true },
+    );
+    const monacoBinding = new MonacoBinding(
+      ytext,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness,
+    );
+  };
+
   return (
     <>
       <main className="h-screen w-full bg-gray-950 flex gap-4 p-3">
@@ -13,6 +37,7 @@ function App() {
             defaultLanguage="javascript"
             defaultValue="// Write your code here"
             theme="vs-dark"
+            onMount={handleMount}
           />
         </section>
       </main>
